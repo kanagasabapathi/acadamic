e9bd529108a23d2bb7c1d244033594aeef10aa81
kanagasabapathi <kanagasabapathi@hotmail.com>
2014-09-15 16:18:01 +0530

    This program describe for ls -a
    ls - list directory contents
     -a, --all
                  do not ignore entries starting with .

--- os/ls-a.c
+++ os/ls-a.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-
-main()
-{
-  FILE *fptr;
-  char line[50]; /* line from unix command*/
-
-  fptr = popen("ls -a","r"); /* Issue the ls command. */
-
-/* Read a line */
-  while ( fgets( line, sizeof line, fptr))
-  {
-    printf("%s", line);
-
- /* or process the 'line' as needed */
+#include <sys/types.h>
+#include <dirent.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  DIR *dir;
+  struct dirent *dirent;
+  char *c = NULL;
+  c = get_current_dir_name();
+  if ((dir = opendir(c))==NULL) {
+    fprintf(stderr,"%d (%s) opendir %s failed\n", errno, strerror(errno), c);
+    return 2;
   }
-  pclose(fptr);
+  while ((dirent = readdir(dir))!=NULL) {
+      printf("%s\n", dirent->d_name);
+  }
+  closedir(dir);
+  return 0;
 }
-
