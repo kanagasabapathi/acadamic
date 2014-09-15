4a6b7918582e2d4affc7fb04a58a5c46e447cde9
kanagasabapathi <kanagasabapathi@hotmail.com>
2014-09-15 16:30:47 +0530

    this program for particular directory ls
    ls filelocation[dir]

--- os/lsdir.c
+++ os/lsdir.c
@@ -4,18 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-
 int main(int argc, char* argv[])
 {
-    DIR *mydir;
-    struct dirent *myfile;
-    struct stat mystat;
-    mydir = opendir(argv[1]);
-    while((myfile = readdir(mydir)) != NULL)
-    {
-	stat(myfile->d_name, &mystat);    
-        printf("%d",mystat.st_size);
-        printf(" %s\n", myfile->d_name);
-    }
-    closedir(mydir);
+        DIR *mydir;
+        struct dirent *myfile;
+        struct stat mystat;
+        mydir = opendir(argv[1]);
+        while((myfile = readdir(mydir)) != NULL){
+                        stat(myfile->d_name, &mystat);
+                        if(myfile->d_name[0]!='.')
+                        printf(" %s\n", myfile->d_name);
+        }
+        closedir(mydir);
 }
